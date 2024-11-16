59b58ec3d30b2a9c2b91f72d1eef2a865b948159
jerryhuangyu <jerry0932619628@gmail.com>
2024-11-16 15:32:34 +0800

    chore: lint with biome

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="min-h-screen w-full items-center justify-center flex">
+		<div className="flex min-h-screen w-full items-center justify-center">
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button
-					type="button"
-					className="bg-green-300 rounded px-2 py-1"
-					onClick={() => setCount((count) => count + 1)}
-				>
+				<button type="button" className="rounded bg-green-300 px-2 py-1" onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 			</div>
